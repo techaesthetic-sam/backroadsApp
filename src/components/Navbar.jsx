9385ffe7a7e08f547518308859b716042d34c50f
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-10-10 13:49:33 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import logo from "../images/logo.svg";
+import { pageLinks } from "../data";
+import { socialLinks } from "../data";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          {/* <img src={image} className="nav-logo" alt="backroads" /> */}
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((el) => {
+            const { id, href, text } = el;
+            return (
+              <li key={id}>
+                <a href={href} className="nav-link">
+                  {text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {/*  */}
+
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a href={href} target="_blank" className="nav-icon">
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import Services from "./Services";
+import Tours from "./Tours";
+import Hero from "./Hero";
+import About from "./About";
+import Footer from "./Footer";
